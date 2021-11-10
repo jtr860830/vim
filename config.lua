fe2b7d404e774814d5b43e555ad4fa4893a7ad6c
Jezrien Hsieh <jtr860830@gmail.com>
2021-11-10 14:41:30 +0800

    Update config for lunarvim

---
+++ config.lua
@@ -0,0 +1,62 @@
+-- general
+vim.opt.expandtab = false
+
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "onedarker"
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup({
+  {
+    exe = "eslint",
+    filetypes = { "javascript" }
+  }
+})
+
+-- formatters
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup({
+  {
+    exe = "eslint",
+    filetypes = { "javascript" }
+  }
+})
+
+-- additional plugin
+lvim.plugins = {
+  { "editorconfig/editorconfig-vim" },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function()
+      require "lsp_signature".setup()
+    end
+  },
+  {
+    "tzachar/cmp-tabnine",
+    run = "./install.sh",
+    event = "InsertEnter",
+  }
+}
